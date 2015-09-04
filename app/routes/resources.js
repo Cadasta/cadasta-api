81c05170dfc235f2d4acd536bcca443f8948326f
Rich Gwozdz <rgwozdz@spatialdev.com>
2015-09-04 14:32:53 -0700

    Resource CRUD.

--- app/routes/resources.js
+++ app/routes/resources.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer();
 var settings = require('../settings/settings.js');
-var AWS = require('aws-sdk');
-AWS.config.update({ accessKeyId: settings.awsAccessKey, secretAccessKey: settings.awsSecretKey });
 
+var AWS = require('aws-sdk');
+AWS.config.update({ accessKeyId: settings.s3.awsAccessKey, secretAccessKey: settings.s3.awsSecretKey });
+
+// CREATE
 router.post('', upload.single('filedata'), function(req, res, next) {
 
     var s3 = new AWS.S3();
@@ -25,38 +27,55 @@ router.post('', upload.single('filedata'), function(req, res, next) {
         res.status(200).json({message: 'Thanks for the upload.'});
     });
 
+});
 
-/*
+// READ
+router.get('/:id', function(req, res, next) {
 
+    //  Use ID to get S3 bucket file path and file name; to be built, just pasting in filename
+    var s3File = 'upload-me.txt';
+    var downloadFileName = "theFile.txt"
 
-    var params = {
-        localFile: req.file,
+    var s3 = new AWS.S3();
 
-        s3Params: {
-            Bucket: "cadasta-test-space",
-            Key: "test-dir/test-upload.txt",
-            // other options supported by putObject, except Body and ContentLength.
-            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-        },
-    };
-    var uploader = client.uploadFile(params);
-    uploader.on('error', function(err) {
-        console.error("unable to upload:", err.stack);
-        res.status(500).json(err);
+    s3.getObject({
+        Bucket: settings.s3.bucket,
+        Key: s3File
+    }, function(err, data) {
+        res.set('Content-disposition', 'attachment; filename=' + downloadFileName);
+        res.set('Content-type', 'text/plain');
+        res.charset = 'UTF-8';
+        res.write(data.Body);
+        res.end();
     });
-    uploader.on('progress', function() {
-        console.log("progress", uploader.progressMd5Amount,
-            uploader.progressAmount, uploader.progressTotal);
-    });
-    uploader.on('end', function() {
-        console.log("done uploading");
-        res.status(200).json({message: 'Thanks for the upload.'});
-    });
-
-*/
-
 
 });
 
+// DELETE
+router.delete('/:id', function(req, res, next) {
+
+    //  Use ID to get S3 bucket file path and file name; to be built, just pasting in filename
+    var s3File = 'upload-me.txt';
+
+    var s3 = new AWS.S3();
+
+    var params = {
+        Bucket: settings.s3.bucket,
+        Key: s3File
+    };
+
+    s3.deleteObject(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            res.status(200).json({message: "File not deleted; " + err})
+
+        }  // error
+        else {
+            console.log(s3File, " deleted.");                 // deleted
+            res.status(200).json({message: "File deleted."});
+        }
+    });
+
+});
 
 module.exports = router;
