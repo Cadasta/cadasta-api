cc416b0aa0ce8e7ce70b8a1a7838259f6e2fdc06
Rich Gwozdz <rgwozdz@spatialdev.com>
2015-10-07 16:51:28 -0700

    Add endpoint /:id/parcels/:parcel_id/resources

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1083,5 +1083,95 @@ router.get('/:id/parcels/:parcel_id/details', common.parseQueryOptions, function
 
 });
 
+/**
+ * @api {get} /parcels/:id/resources Get parcel resources
+ * @apiName GetParcelResources
+ * @apiGroup Parcels
+ *
+ * @apiDescription Get all parcel resources (from the resource_parcel table)
+ *
+ * @apiParam (Optional query string parameters) {String} [fields] Options: id, user_id, time_created, time_updated
+ * @apiParam (Optional query string parameters) {String} [sort_by] Options: id, user_id, time_created, time_updated
+ * @apiParam (Optional query string parameters) {String} [sort_dir=ASC] Options: ASC or DESC
+ * @apiParam (Optional query string parameters) {Number} [limit] integer of records to return
+ *
+ * @apiSuccess {Object} response A feature collection with zero to many features
+ * @apiSuccess {String} response.type "Feature Collection"
+ * @apiSuccess {Object[]} response.features An array of feature objects
+ * @apiSuccess {String} response.features.type "Feature"
+ * @apiSuccess {Object} response.features.geometry GeoJSON geometry object
+ * @apiSuccess {Object} response.features.properties GeoJSON feature's properties
+ * @apiSuccess {Integer} response.features.properties.resource_id resource id
+ * @apiSuccess {Integer} response.features.properties.parcel_id resource parcel id
+ * @apiSuccess {Integer} response.features.properties.project_id resource project id
+ * @apiSuccess {String} response.features.properties.type resource type (parcel,party,relationship)
+ * @apiSuccess {String} response.features.properties.url resource download url
+ * @apiSuccess {String} response.features.properties.description resource description
+ * @apiSuccess {String} response.features.properties.time_created Time stamp of creation
+ * @apiSuccess {String} response.features.properties.time_updated Time stamp of last update
+ * @apiSuccess {Number} response.features.properties.created_by id of creator
+ * @apiSuccess {Number} response.features.properties.updated_by id of updater
+ *
+ * @apiExample {curl} Example usage:
+ *     curl -i http://localhost/parcels/1/resources
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": null,
+            "properties": {
+                "parcel_id": 1,
+                "resource_id": 32,
+                "type": null,
+                "url": "http://www.cadasta.org/32/parcel",
+                "description": null,
+                "active": true,
+                "sys_delete": false,
+                "time_created": "2015-09-09T14:57:34.398855-07:00",
+                "time_updated": "2015-09-09T14:57:34.398855-07:00",
+                "created_by": null,
+                "updated_by": null,
+                "project_id": 1
+            }
+        }
+    ]
+}
+ */
+router.get('/:id/parcels/:parcel_id/resources', common.parseQueryOptions, function(req, res, next) {
+
+    var whereClauseArr = [];
+    var whereClauseValues = [];
+
+    var options =  {
+        queryModifiers: req.queryModifiers,
+        outputFormat: 'GeoJSON'
+    };
+
+    var whereClauseArr = ['project_id = $1', 'parcel_id = $2'];
+    var whereClauseValues = [req.params.id, req.params.parcel_id];
+
+    var options =  {
+        queryModifiers: req.queryModifiers,
+        outputFormat: req.query.outputFormat || 'GeoJSON'
+    };
+
+    options.whereClause ='WHERE ' + whereClauseArr.join(' AND ');
+    options.whereClauseValues = whereClauseValues;
+
+    ctrlCommon.getAll("show_parcel_resources", options)
+        .then(function(result){
+            res.status(200).json(result[0].response);
+        })
+        .catch(function(err){
+            next(err);
+        })
+        .done();
+
+});
+
 
 module.exports = router;
