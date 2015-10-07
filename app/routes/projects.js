0c5ac68b3bda0ea645707c8f0338f599e6975ca9
Rich Gwozdz <rgwozdz@spatialdev.com>
2015-10-07 13:26:37 -0700

    Add projects/:id/parcels/:id/history endpoint.

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -609,7 +609,7 @@ router.get('/:id/parcels_list', common.parseQueryOptions, function(req, res, nex
  * @apiSuccess {String} response.features.properties.time_created Time stamp of last update
  *
  * @apiExample {curl} Example usage:
- *     curl -i http://localhost/project/1/parcels/1
+ *     curl -i http://localhost/projects/1/parcels/1
  *
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
@@ -689,6 +689,103 @@ router.get('/:id/parcels/:parcel_id', common.parseQueryOptions, function(req, re
 
 });
 
+/**
+ * @api {get} projects/:id/parcels/:id/history Project parcel history
+ * @apiName GetProjectParcelHistory
+ * @apiGroup Projects
+ *
+ * @apiDescription Get a project parcel's parcel history (from the parcel_history table)
+ *
+ * @apiParam {Number} id project's unique ID.
+ * @apiParam {Number} parcel_id parcel's unique ID.
+ *
+ * @apiParam (Optional query string parameters) {String} [fields] Options: id, spatial_source, user_id, time_created, time_updated
+ * @apiParam (Optional query string parameters) {String} [sort_by] Options: id, spatial_source, user_id, time_created, time_updated
+ * @apiParam (Optional query string parameters) {String} [sort_dir=ASC] Options: ASC or DESC
+ * @apiParam (Optional query string parameters) {Number} [limit] integer of records to return
+ *
+ * @apiSuccess {Object} response A feature collection with one feature per parcel history record
+ * @apiSuccess {String} response.type "Feature Collection"
+ * @apiSuccess {Object[]} response.features An array of feature objects
+ * @apiSuccess {String} response.features.type "Feature"
+ * @apiSuccess {Object} response.features.geometry GeoJSON geometry object (always null here)
+ * @apiSuccess {Object} response.features.properties GeoJSON feature's properties
+ * @apiSuccess {Number} response.features.properties.id history id
+ * @apiSuccess {Number} response.features.properties.parcel_id parcel id
+ * @apiSuccess {Number} response.features.properties.origin_id origin id
+ * @apiSuccess {Number} response.features.properties.parent_id parent id
+ * @apiSuccess {String} response.features.properties.version version
+ * @apiSuccess {String} response.features.properties.description description
+ * @apiSuccess {String} response.features.properties.date_modified YYYY-MM-DD of last update
+ * @apiSuccess {Boolean} response.features.properties.active active/archived flag
+ * @apiSuccess {String} response.features.properties.time_created Time stamp of creation
+ * @apiSuccess {String} response.features.properties.time_updated Time stamp of last update
+ * @apiSuccess {Number} response.features.properties.created_by id of creator
+ * @apiSuccess {Number} response.features.properties.updated_by id of updater
+ *
+ * @apiExample {curl} Example usage:
+ *     curl -i http://localhost/projects/1/parcels/1/history
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+          "type": "FeatureCollection",
+          "features": [
+            {
+              "type": "Feature",
+              "geometry": null,
+              "properties": {
+                "id": 1,
+                "parcel_id": 1,
+                "origin_id": 1,
+                "parent_id": null,
+                "version": 1,
+                "description": "new description",
+                "date_modified": "2015-08-31",
+                "active": true,
+                "time_created": "2015-08-31T13:00:18.339167-07:00",
+                "time_updated": null,
+                "created_by": 11,
+                "updated_by": null
+              }
+            }
+          ]
+        }
+ */
+router.get('/:id/parcels/:parcel_id/history', common.parseQueryOptions, function(req, res, next) {
 
+    req.queryModifiers.returnGeometry = false;
+
+    var options =  {
+        queryModifiers: req.queryModifiers,
+        outputFormat: 'GeoJSON'
+    };
+
+    var whereClauseArr = ['project_id = $1', 'id = $2'];
+    var whereClauseValues = [req.params.id, req.params.parcel_id];
+
+    var options =  {
+        queryModifiers: req.queryModifiers,
+        outputFormat: req.query.outputFormat || 'GeoJSON'
+    };
+
+    if(whereClauseArr.length > 0) {
+        options.whereClause = 'WHERE ' + whereClauseArr.join(' AND ');
+        options.whereClauseValues = whereClauseValues;
+    }
+
+
+    ctrlCommon.getWithId('show_parcel_history', 'parcel_id', req.params.id, {queryModifiers: req.queryModifiers, outputFormat: 'GeoJSON'})
+        .then(function(result){
+
+            res.status(200).json(result[0].response);
+
+        })
+        .catch(function(err){
+            next(err);
+        })
+        .done();
+
+});
 
 module.exports = router;
