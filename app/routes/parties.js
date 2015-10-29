4a748e1a17d5b628fef735c9b758207304ac2f95
Daniel Baah <dbaah@spatialdev.com>
2015-10-29 14:26:23 -0700

    party details endpoint & test

--- app/routes/parties.js
+++ app/routes/parties.js
@@ -5,7 +5,206 @@ var ctrlCommon = require('../controllers/common.js');
 var Q = require('q');
 var pgb = require('../pg-binding.js');
 
-// CREATE A PROJECT RECORD
+
+/**
+ * @api {get} /projects/:project_id/parties/:party_id/details Project parties - details
+ * @apiName GetProjectPartyDetails
+ * @apiGroup Projects
+ * @apiDescription Get all details for a project party attributes, relationships , parcels (most recent 10)
+ *
+ * @apiParam {Number} id parties unique ID.
+ * @apiParam {Number} project_id parties unique project ID.
+ *
+ *
+ * @apiSuccess {Object} response A feature collection with one feature representing a parcel
+ * @apiSuccess {String} response.type "Feature Collection"
+ * @apiSuccess {Object[]} response.features An array of feature objects
+ * @apiSuccess {String} response.features.type "Feature"
+ * @apiSuccess {Object} response.features.geometry GeoJSON geometry object (always null here)
+ * @apiSuccess {Object} response.features.properties GeoJSON feature's properties
+ * @apiSuccess {Integer} response.features.properties.id party id
+ * @apiSuccess {Integer} response.features.properties.project_id party project id
+ * @apiSuccess {Integer} response.features.properties.num_relationships number of party relationships
+ * @apiSuccess {String} response.features.properties.first_name party first name
+ * @apiSuccess {String} response.features.properties.last_name party last name
+ * @apiSuccess {String} response.features.properties.group_name Time stamp of last update
+ * @apiSuccess {Timestamp} response.features.properties.time_created timestamp of party creation
+ * @apiSuccess {Timestamp} response.features.properties.time_updated timestamp of party update
+ * @apiSuccess {Array} response.features.properties.parcels
+ * @apiSuccess {Number} response.features.properties.parcels.geometry parcels GeoJSON geometry object
+ * @apiSuccess {Object} response.features.properties.parcels.properties GeoJSON feature's properties
+ * @apiSuccess {Number} response.features.properties.parcels.properties.project_id project id
+ * @apiSuccess {Number} response.features.properties.parcels.properties.parcel_id parcel id
+ * @apiSuccess {Number} response.features.properties.parcels.properties.party_id party_id id
+ * @apiSuccess {String} response.features.properties.parcels.properties.relationship_id relationship id
+ * @apiSuccess {Array} response.features.properties.relationships
+ * @apiSuccess {Number} response.features.properties.relationships.geometry relationships GeoJSON geometry object
+ * @apiSuccess {Object} response.features.properties.relationships.properties GeoJSON feature's properties
+ * @apiSuccess {Number} response.features.properties.relationships.properties.id relationship id
+ * @apiSuccess {String} response.features.properties.relationships.properties.tenure_type
+ * @apiSuccess {String} response.features.properties.relationships.properties.how_acquired
+ * @apiSuccess {Date} response.features.properties.relationships.properties.acquired_date
+ * @apiSuccess {Number} response.features.properties.relationships.properties.parcel_id
+ * @apiSuccess {Number} response.features.properties.relationships.properties.project_id
+ * @apiSuccess {String} response.features.properties.relationships.properties.spatial_source
+ * @apiSuccess {Number} response.features.properties.relationships.properties.party_id
+ * @apiSuccess {String} response.features.properties.relationships.properties.first_name
+ * @apiSuccess {String} response.features.properties.relationships.properties.last_name
+ * @apiSuccess {Timestamp} response.features.properties.relationships.properties.time_created
+ * @apiSuccess {Timestamp} response.features.properties.relationships.properties.time_updated
+ * @apiSuccess {Number} response.features.properties.relationships.properties.active
+ *
+ * @apiExample {curl} Example usage:
+ *     curl -i http://localhost/projects/1/parties/1/details
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": null,
+            "properties": {
+                "project_id": 1,
+                "id": 2,
+                "num_relationships": 2,
+                "group_name": null,
+                "first_name": "Oscar",
+                "last_name": "Sanders ",
+                "type": "individual",
+                "active": true,
+                "time_created": "2015-10-28T18:52:00.69074-07:00",
+                "time_updated": "2015-10-28T18:52:00.69074-07:00",
+                "parcels": [
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Polygon",
+                            "coordinates": [
+                                [
+                                    [
+                                        -68.1227874755859,
+                                        -16.498764239693
+                                    ],
+                                    [
+                                        -68.1223583221435,
+                                        -16.4987230913468
+                                    ],
+                                    [
+                                        -68.1221222877502,
+                                        -16.4989699812929
+                                    ],
+                                    [
+                                        -68.1222081184387,
+                                        -16.4998135195646
+                                    ],
+                                    [
+                                        -68.1224226951599,
+                                        -16.5002661483402
+                                    ],
+                                    [
+                                        -68.1230235099792,
+                                        -16.5002250003135
+                                    ],
+                                    [
+                                        -68.1231951713562,
+                                        -16.4996077788619
+                                    ],
+                                    [
+                                        -68.1231737136841,
+                                        -16.4990317037302
+                                    ],
+                                    [
+                                        -68.1227874755859,
+                                        -16.498764239693
+                                    ]
+                                ]
+                            ]
+                        },
+                        "properties": {
+                            "project_id": 1,
+                            "parcel_id": 2,
+                            "party_id": 2,
+                            "relationship_id": 2
+                        }
+                    }
+                ],
+                "relationships": [
+                    {
+                        "type": "Feature",
+                        "geometry": null,
+                        "properties": {
+                            "id": 2,
+                            "tenure_type": "own",
+                            "how_acquired": "lease",
+                            "acquired_date": "2014-09-09",
+                            "parcel_id": 2,
+                            "project_id": 1,
+                            "spatial_source": "survey_sketch",
+                            "party_id": 2,
+                            "first_name": "Oscar",
+                            "last_name": "Sanders ",
+                            "time_created": "2015-10-28T18:52:00.69074-07:00",
+                            "active": true,
+                            "time_updated": "2015-10-28T18:52:00.69074-07:00"
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+}
+ */
+router.get('/:project_id/parties/:id/details', common.parseQueryOptions, function(req, res, next) {
+
+    var whereClauseArr = ['project_id = $1', 'party_id = $2'];
+    var whereClauseValues = [req.params.project_id, req.params.id];
+
+    //geometry options for show_party_parcels
+    var geomOptions =  {
+        queryModifiers: {returnGeometry: true},
+        outputFormat: 'GeoJSON'
+    };
+
+    geomOptions.whereClause = 'WHERE ' + whereClauseArr.join(' AND ');
+    geomOptions.whereClauseValues = whereClauseValues;
+
+    //party options
+    var partyWhereClauseArr = ['project_id = $1', 'id = $2'];
+    var partyWhereClauseValues = [req.params.project_id, req.params.id];
+
+    var partyOptions =  {
+        queryModifiers: req.queryModifiers,
+        outputFormat: 'GeoJSON'
+    };
+
+    partyOptions.whereClause ='WHERE ' + partyWhereClauseArr.join(' AND ');
+    partyOptions.whereClauseValues = partyWhereClauseValues;
+
+    Q.all([
+        ctrlCommon.getAll('show_parties',  partyOptions),
+        ctrlCommon.getAll('show_party_parcels',  geomOptions),
+        ctrlCommon.getAll('show_relationships', geomOptions)
+    ])
+        .then(function (results) {
+
+            //Process results: Add parcel history and relationships to Parcel GeoJSON
+            var geoJSON = results[0][0].response;
+
+            // Add properties to parties geojson
+            geoJSON.features[0].properties.parcels = results[1][0].response.features;
+            geoJSON.features[0].properties.relationships = results[2][0].response.features;
+
+            res.status(200).json(geoJSON);
+        })
+        .catch(function(err){
+            next(err)
+        })
+        .done();
+});
+
+// CREATE A PARTY RECORD
 /**
  * @api {post} /projects/1/parties Project parties - Create one
  * @apiName PostParties
--- test/endpointSpecs/partiesSpec.js
+++ test/endpointSpecs/partiesSpec.js
@@ -73,7 +73,84 @@ module.exports = function(app) {
 
             });
         });
+        describe('GET /projects/3/parties/1/details', function () {
+            it('should have status 200 and contain specified data structure', function (done) {
 
+                chai.request(app)
+                    .get('/projects/1/parties/1/details')
+                    .end(function (res) {
+
+                        // Test that the endpoint exists and responds
+                        expect(res).to.have.status(200);
+
+                        // Test that it returns GeoJSON
+                        testUtils.expectFeatureCollection(res.body);
+
+                        //  Get the GeoJSON features for further testing
+                        var features = res.body.features;
+
+                        // Make sure only one feature returned
+                        expect(features).with.length(1);
+
+                        // Check properties
+                        var featureProperties = features[0].properties;
+
+                        expect(featureProperties).to.have.property('id');
+                        expect(featureProperties).to.have.property('project_id');
+                        expect(featureProperties).to.have.property('num_relationships');
+                        expect(featureProperties).to.have.property('group_name');
+                        expect(featureProperties).to.have.property('first_name');
+                        expect(featureProperties).to.have.property('last_name');
+                        expect(featureProperties).to.have.property('type');
+                        expect(featureProperties).to.have.property('active');
+                        expect(featureProperties).to.have.property('time_created');
+                        expect(featureProperties).to.have.property('time_updated');
+
+                        var parcels = featureProperties.parcels[0].properties;
+                        expect(parcels).to.have.property('project_id');
+                        expect(parcels).to.have.property('parcel_id');
+                        expect(parcels).to.have.property('party_id');
+                        expect(parcels).to.have.property('relationship_id');
+
+                        var relationships = featureProperties.relationships[0].properties;
+                        expect(relationships).to.have.property('id');
+                        expect(relationships).to.have.property('tenure_type');
+                        expect(relationships).to.have.property('how_acquired');
+                        expect(relationships).to.have.property('acquired_date');
+                        expect(relationships).to.have.property('parcel_id');
+                        expect(relationships).to.have.property('project_id');
+                        expect(relationships).to.have.property('spatial_source');
+                        expect(relationships).to.have.property('party_id');
+                        expect(relationships).to.have.property('first_name');
+                        expect(relationships).to.have.property('last_name');
+                        expect(relationships).to.have.property('time_created');
+                        expect(relationships).to.have.property('active');
+                        expect(relationships).to.have.property('time_updated');
+
+                        done();
+                    });
+
+            });
+        });
+        describe('POST /projects/3/parties', function () {
+            it('should have status 200 and contain specified data structure', function (done) {
+
+                chai.request(app)
+                    .post('/projects/1/parties')
+                    .send({"first_name": null, "last_name": null, "group_name": "Wallys World", "party_type": "group"})
+                    .end(function (res) {
+
+                        //Test that the endpoint exists and responds
+                        expect(res).to.have.status(200);
+
+                        expect(res.body).to.have.property('cadasta_party_id');
+
+                        done();
+                    });
+
+            });
+        });
+        
     })
 
 };
