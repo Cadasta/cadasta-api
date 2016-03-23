499f3ba21de3843e0d2156a17137bcdc50d17a77
Brian O'Hare <bjohare@gmail.com>
2016-03-23 12:53:21 +0000

    Adding reusable pagination function.

---
+++ app/pagination.js
@@ -0,0 +1,29 @@
+var pagination = {}
+
+/**
+ * Adds pagination related headers to the response.
+ *
+ * @param req   the request
+ * @param res   the response
+ * @param count the total result count
+ */
+pagination.addPaginationHeaders = function(req, res, count){
+
+    var limit = req.query.limit;
+    var offset = req.query.offset;
+    var project_id = req.params.id;
+    var start_idx = 0;
+    var end_idx = 0;
+
+    if (limit && offset){
+        limit = parseInt(limit);
+        offset = parseInt(offset);
+        start_idx = offset + 1;
+        end_idx = limit + offset;
+        var count = parseInt(count) -1;
+        end_idx = end_idx < count ? end_idx : count;
+        res.set('Content-Range', start_idx + '-' + end_idx + '/' + count);
+    }
+}
+
+module.exports = pagination;
